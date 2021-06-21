9d6a5a63e2a5bf8bbdcf213237e3c75101658c50
JoseIgnacioTapia <josignaciot@gmail.com>
2021-06-21 20:59:37 -0300

    Displaying Artist Information

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 import Cancion from './components/Cancion';
+import Info from './components/Info';
 
 import axios from 'axios';
 
@@ -8,7 +9,7 @@ function App() {
   // Definir el state
   const [busquedaletra, setBusquedaletra] = useState({});
   const [letra, setLetra] = useState('');
-  const [informacion, setInformacion] = useState('');
+  const [informacion, setInformacion] = useState({});
 
   useEffect(() => {
     if (Object.keys(busquedaletra).length === 0) return;
@@ -30,14 +31,16 @@ function App() {
     }
 
     consultarApiLetra();
-  }, [busquedaletra]);
+  }, [busquedaletra, informacion]);
 
   return (
     <>
       <Formulario setBusquedaletra={setBusquedaletra} />
       <div className="container mt-5">
         <div className="row">
-          <div className="col-md-6"></div>
+          <div className="col-md-6">
+            <Info informacion={informacion} />
+          </div>
           <div className="col-md-6">
             <Cancion letra={letra} />
           </div>
